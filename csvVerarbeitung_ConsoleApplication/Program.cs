485285948d378b5033b1ee785446e11dbb954e51
programmiertier <zimmermannveronique@protonmail.com>
2017-11-03 13:55:51 +0100

    plz und orte

--- csvVerarbeitung_ConsoleApplication/Program.cs
+++ csvVerarbeitung_ConsoleApplication/Program.cs
@@ -18,16 +18,7 @@ namespace csvVerarbeitung_ConsoleApplication
 
         static void Main(string[] args)
         {
-            // ein Objekt mit Daten erstellen
-            // PLZ dieDaten = new PLZ();
-            // WriteLine(dieDaten.ortsangaben[0].id);  // geht nur, wenn ort public gemacht ist
             string[] csvZeilen = File.ReadAllLines("plz.csv");
-            /*for (int zaehler = 0; zaehler < csvZeilen.Length; zaehler++)
-            {
-                WriteLine("{0}", (csvZeilen[zaehler].Split(','))[2]);
-            }
-            warten();*/
-
             var query = from csvZeile in csvZeilen
                         let ortsdaten = csvZeile.Split(',')
                         select new
@@ -40,22 +31,10 @@ namespace csvVerarbeitung_ConsoleApplication
                             landnr = ortsdaten[5],
                             bundesland = ortsdaten[6]
                         };
-            foreach (var item in query)
+            foreach ( var item in query )
             {
-                WriteLine("Orte mit Postleitzahlen: {0}\t{1}", item.ort, item.plz);
+                WriteLine("Postleitzahlen mit Orten: {0} \t{1}", item.plz, item.ort);
             }
-
-            // Abfragen LINQ an das Objekt richten
-
-            // alle vorhandenen Orte nennen
-
-            // macht das selbe
-            /* var alleOrte = (from alles in dieDaten.ortsangaben orderby alles.ort select alles.ort).Distinct();
-            // var alleOrte = (from alles in dieDaten.ortsangaben select alles.ort).Distinct().OrderBy(platzhalter => platzhalter);
-            foreach ( var item in alleOrte )
-            {
-                WriteLine(item);
-            }   */
             warten();
         }
     }
