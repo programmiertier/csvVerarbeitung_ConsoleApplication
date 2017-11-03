ec3043479f3b464414acf9be2c33fb62227f49ae
programmiertier <zimmermannveronique@protonmail.com>
2017-11-03 13:09:56 +0100

    aus der csv Datei lesen

--- csvVerarbeitung_ConsoleApplication/Program.cs
+++ csvVerarbeitung_ConsoleApplication/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,21 +19,25 @@ namespace csvVerarbeitung_ConsoleApplication
         static void Main(string[] args)
         {
             // ein Objekt mit Daten erstellen
-
-            PLZ dieDaten = new PLZ();
+            // PLZ dieDaten = new PLZ();
             // WriteLine(dieDaten.ortsangaben[0].id);  // geht nur, wenn ort public gemacht ist
-
+            string[] csvZeilen = File.ReadAllLines("plz.csv");
+            for (int zaehler = 0; zaehler < csvZeilen.Length; zaehler++)
+            {
+                WriteLine("{0}", csvZeilen[zaehler].Split(',')[0]);
+            }
+            warten();
             // Abfragen LINQ an das Objekt richten
 
             // alle vorhandenen Orte nennen
 
             // macht das selbe
-            // var alleOrte = (from alles in dieDaten.ortsangaben orderby alles.ort select alles.ort).Distinct();
-            var alleOrte = (from alles in dieDaten.ortsangaben select alles.ort).Distinct().OrderBy(platzhalter => platzhalter);
+            /*var alleOrte = (from alles in dieDaten.ortsangaben orderby alles.ort select alles.ort).Distinct();
+            // var alleOrte = (from alles in dieDaten.ortsangaben select alles.ort).Distinct().OrderBy(platzhalter => platzhalter);
             foreach ( var item in alleOrte )
             {
                 WriteLine(item);
-            }
+            }*/
             warten();
         }
     }
